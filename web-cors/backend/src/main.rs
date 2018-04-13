8afcbcd1e270dbb5a869a144367dacc0906d8d76
krircc <krircc@aliyun.com>
2018-04-13 16:03:58 +0800

    update web-cors

--- web-cors/backend/src/main.rs
+++ web-cors/backend/src/main.rs
@@ -7,7 +7,7 @@ extern crate actix_web;
 extern crate env_logger;
 
 use std::env;
-use actix_web::{http, middleware, server, App};
+use actix_web::{server, App, http::{header, Method}, middleware, middleware::cors::Cors};
 
 mod user;
 use user::info;
@@ -20,23 +20,18 @@ fn main() {
     let sys = actix::System::new("Actix-web-CORS");
 
     server::new(
-        || App::new()
+        move || App::new()
             .middleware(middleware::Logger::default())
-            .resource("/user/info", |r| {
-                middleware::cors::Cors::build()
-                    .allowed_origin("http://localhost:1234")
-                    .allowed_methods(vec!["GET", "POST"])
-                    .allowed_headers(
-                        vec![http::header::AUTHORIZATION,
-                             http::header::ACCEPT,
-                             http::header::CONTENT_TYPE])
-                    .max_age(3600)
-                    .finish().expect("Can not create CORS middleware")
-                    .register(r);
-                r.method(http::Method::POST).a(info);
-            }))
+            .configure(|app| Cors::for_app(app)
+            .allowed_origin("http://localhost:1234")
+            .allowed_methods(vec!["GET", "POST"])
+            .allowed_headers(vec![header::AUTHORIZATION, header::ACCEPT])
+            .allowed_header(header::CONTENT_TYPE)
+            .max_age(3600)
+            .resource("/user/info", |r| { r.method(Method::POST).with(info);})
+            .register()))
         .bind("127.0.0.1:8000").unwrap()
-        .shutdown_timeout(200)
+        .shutdown_timeout(2)
         .start();
 
     let _ = sys.run();
