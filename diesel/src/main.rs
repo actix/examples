66906c8aa86819b8accba05e2cce2f35e945242e
William Brown <william@blackhats.net.au>
2018-11-18 19:56:22 +1000

    Update examples
    Update examples

--- diesel/src/main.rs
+++ diesel/src/main.rs
@@ -17,13 +17,14 @@ extern crate futures;
 extern crate r2d2;
 extern crate uuid;
 extern crate bytes;
+extern crate json;
 
 
 use bytes::BytesMut;
 use actix::prelude::*;
 use actix_web::{
     http, middleware, server, App, AsyncResponder, FutureResponse, HttpResponse, Path, Error, HttpRequest,
-    State, HttpMessage, error
+    State, HttpMessage, error, Json
 };
 
 use diesel::prelude::*;
@@ -113,17 +114,20 @@ fn index_add(req: &HttpRequest<AppState>) -> Box<Future<Item = HttpResponse, Err
         .responder()
 }
 
-fn extract_item_limit((item, req): (Json<MyUser>, HttpRequest<AppState>)) -> HttpResponse {
-    req.state()
+fn add2((item, req): (Json<MyUser>, HttpRequest<AppState>)) -> Box<Future<Item = HttpResponse, Error = Error>> {
+    Box::new(req.state()
         .db
         .send(CreateUser {
-            name: item.name,
+            // Is it possible to do with without cloning? Apparently this is "borrowed".
+            // A risk is that when this is sent as a message, the lifetimes will
+            // be very complex to handle, and may not be reasonable ...
+            name: item.name.clone(),
         })
         .from_err()
         .and_then(|res| match res {
-            Ok(user) => HttpResponse::Ok().json(user),
-            Err(_) => HttpResponse::InternalServerError().into(),
-        })
+            Ok(user) => Ok(HttpResponse::Ok().json(user)),
+            Err(_) => Ok(HttpResponse::InternalServerError().into()),
+        }))
 }
 
 fn main() {
@@ -148,11 +152,36 @@ fn main() {
             // curl --header "Content-Type: application/json" --request POST --data '{"name":"xyz"}'  http://127.0.0.1:8080/add
             .resource("/add2", |r| {
                 r.method(http::Method::POST)
-                    .with_config(extract_item_limit, |((cfg, _),)| {
-                        cfg.limit(4096); // <- limit size of the payload
+                    // MENTAL NOTE: All error types don't seem to display error content on POST?
+                    //
+                    // No example ever actually uses with_config with a tuple cfg/more
+                    //  As a result, there is no example of what type cfg should be that
+                    //  I can find, so disabling cfg as there is no way to derive this type.
+                    //  This means this usage is *not* production safe (lack of rate limit
+                    //  may lead to DoS)
+                    // No way to use with_state and with_config at the same time.
+                    // No way to return *what* decode error actually occured to caller
+                    //   to help them fix their request. IE if you are missing a field
+                    //   we should inform *what* is missingg.
+                    //   (Today you get a blank result ... :( )
+                    // extractor doesn't use serde_json (application may
+                    //  wish to standardise on a single json parser). This doesn't
+                    //  mean the extractor is bad, but that it's not going to solve
+                    //  all problems and use cases. Manual extraction will be important.
+                    // Extractor is limited to json (may want other types, see manual
+                    //  below).
+                    //
+                    // This does not work
+                    .with_config(add2, |cfg| {
+                        cfg.0.limit(4096); // <- limit size of the payload
                     })
+                    // .with(add2) <-- this works
             })
-            .resource("/add", |r| r.method(http::Method::POST).f(index_add))
+            //  Manual parsing would allow custom error construction, use of
+            //  other parsers *beside* json (for example CBOR, protobuf, xml), and allows
+            //  an application to standardise on a single parser implementation.
+            //  It's important that this is fixed as a working example.
+            // .resource("/add", |r| r.method(http::Method::POST).f(index_add))
             .resource("/add/{name}", |r| r.method(http::Method::GET).with(add))
     }).bind("127.0.0.1:8080")
         .unwrap()
