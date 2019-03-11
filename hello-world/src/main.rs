d9e7902e8891f4535be9bf849a9199ad832ecb9f
Tommaso Allevi <tomallevi@gmail.com>
2019-03-11 23:49:42 +0100

    Add test to hello-world

--- hello-world/src/main.rs
+++ hello-world/src/main.rs
@@ -19,3 +19,33 @@ fn main() -> std::io::Result<()> {
     .bind("127.0.0.1:8080")?
     .run()
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use actix_web::dev::Body::Bytes;
+    use actix_web::dev::ResponseBody::{Other};
+    use actix_web::HttpResponse;
+    use actix_web::Error;
+    use actix_web::{http, test};
+
+     #[test]
+    fn test_index() -> Result<(), Error>  {
+        let response: HttpResponse = test::TestRequest::default()
+            .run(&index)?;
+        assert_eq!(response.status(), http::StatusCode::OK);
+
+         let response_body = match response.body() {
+            Other(body) => match body {
+                Bytes(bytes) => String::from_utf8(bytes.to_vec()).unwrap(),
+                _ => panic!("Unknow body type: #1")
+            },
+            _ => panic!("Unknow body type: #2")
+        };
+
+         assert_eq!(response_body, r##"Hello world!"##);
+
+         Ok(())
+    }
+}
