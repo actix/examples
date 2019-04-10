4be146424912cc86e087d6e1989807bbc09dbd98
dowwie <dkcdkg@gmail.com>
2019-04-10 14:08:10 -0400

    added unit test example to async_ex2

--- async_ex2/src/handlers/products.rs
+++ async_ex2/src/handlers/products.rs
@@ -24,3 +24,34 @@ pub fn remove_product(id: web::Path<String>)
                    -> impl Future<Item = HttpResponse, Error = Error> {
     fut_ok(HttpResponse::Ok().finish())
 }
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use actix_service::Service;
+    use actix_web::{test, HttpResponse, HttpRequest, App,
+                    http::{header, StatusCode}, web};
+    use crate::appconfig::config_app;
+
+
+    #[test]
+    fn test_add_product() {
+        let mut app = test::init_service(
+           App::new()
+               .configure(config_app)
+        );
+
+        let payload = r#"{"id":12345,"product_type":"fancy","name":"test"}"#.as_bytes();
+
+        let req = test::TestRequest::post()
+                            .uri("/products")
+                            .header(header::CONTENT_TYPE, "application/json")
+                            .set_payload(payload)
+                            .to_request();
+
+        let resp = test::block_on(app.call(req)).unwrap();
+
+        assert_eq!(resp.status(), StatusCode::OK);
+    }
+}
