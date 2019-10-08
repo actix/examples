694326f3c9d4d95c09ccd43db8501487dec52abe
Tommaso Allevi <tomallevi@gmail.com>
2019-10-09 00:21:11 +0200

    Add test in hello world example

--- hello-world/src/main.rs
+++ hello-world/src/main.rs
@@ -19,3 +19,32 @@ fn main() -> std::io::Result<()> {
     .bind("127.0.0.1:8080")?
     .run()
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use actix_web::dev::Service;
+    use actix_web::{test, web, App, http, Error};
+
+    #[test]
+    fn test_index() -> Result<(), Error>  {
+        let app = App::new().route("/", web::get().to(index));
+        let mut app = test::init_service(app);
+
+        let req = test::TestRequest::get()
+            .uri("/")
+            .to_request();
+        let resp = test::block_on(app.call(req)).unwrap();
+
+        assert_eq!(resp.status(), http::StatusCode::OK);
+
+        let response_body = match resp.response().body().as_ref() {
+            Some(actix_web::body::Body::Bytes(bytes)) => bytes,
+            _ => panic!("Response error"),
+        };
+
+        assert_eq!(response_body, r##"Hello world!"##);
+
+        Ok(())
+    }
+}
