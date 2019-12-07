e14f8cf0bc556266472107e63de9b60af20d821c
Jens Krause <email@jkrause.io>
2019-12-07 18:49:47 +0100

    Update Juniper example

--- juniper/README.md
+++ juniper/README.md
@@ -1,6 +1,8 @@
-# juniper
+# Juniper
 
-Juniper integration for Actix web
+[Juniper](https://github.com/graphql-rust/juniper) integration for Actix web
+
+## Usage
 
 ### server
 
@@ -13,3 +15,57 @@ cargo run (or ``cargo watch -x run``)
 ### web client
 
 [http://127.0.0.1:8080/graphiql](http://127.0.0.1:8080/graphiql)
+
+_Query example:_
+```graphql
+{
+  human(id: "1234") {
+    name
+    appearsIn
+    homePlanet
+  }
+}
+```
+_Result:_
+```json
+{
+  "data": {
+    "human": {
+      "name": "Luke",
+      "appearsIn": [
+        "NEW_HOPE"
+      ],
+      "homePlanet": "Mars"
+    }
+  }
+}
+```
+
+_Mutation example:_
+
+```graphql
+mutation {
+  createHuman(newHuman: {name: "Fresh Kid Ice", appearsIn: EMPIRE, homePlanet: "earth"}) {
+    id
+    name
+    appearsIn
+    homePlanet
+  }
+}
+```
+
+_Result:_
+```json
+{
+  "data": {
+    "createHuman": {
+      "id": "1234",
+      "name": "Fresh Kid Ice",
+      "appearsIn": [
+        "EMPIRE"
+      ],
+      "homePlanet": "earth"
+    }
+  }
+}
+```
--- juniper/src/main.rs
+++ juniper/src/main.rs
@@ -8,7 +8,6 @@ use std::sync::Arc;
 extern crate juniper;
 
 use actix_web::{middleware, web, App, Error, HttpResponse, HttpServer};
-use futures::future::Future;
 use juniper::http::graphiql::graphiql_source;
 use juniper::http::GraphQLRequest;
 
@@ -17,43 +16,40 @@ mod schema;
 use crate::schema::{create_schema, Schema};
 
 fn graphiql() -> HttpResponse {
-    let html = graphiql_source("http://127.0.0.1:8080/graphql");
-    HttpResponse::Ok()
-        .content_type("text/html; charset=utf-8")
-        .body(html)
+  let html = graphiql_source("http://127.0.0.1:8080/graphql");
+  HttpResponse::Ok()
+    .content_type("text/html; charset=utf-8")
+    .body(html)
 }
 
 fn graphql(
-    st: web::Data<Arc<Schema>>,
-    data: web::Json<GraphQLRequest>,
-) -> impl Future<Item = HttpResponse, Error = Error> {
-    web::block(move || {
-        let res = data.execute(&st, &());
-        Ok::<_, serde_json::error::Error>(serde_json::to_string(&res)?)
-    })
-    .map_err(Error::from)
-    .and_then(|user| {
-        Ok(HttpResponse::Ok()
-            .content_type("application/json")
-            .body(user))
-    })
+  st: web::Data<Arc<Schema>>,
+  data: web::Json<GraphQLRequest>,
+) -> Result<HttpResponse, Error> {
+  let res = data.execute(&st, &());
+  let user = serde_json::to_string(&res)?;
+  Ok(
+    HttpResponse::Ok()
+      .content_type("application/json")
+      .body(user)
+  )
 }
 
 fn main() -> io::Result<()> {
-    std::env::set_var("RUST_LOG", "actix_web=info");
-    env_logger::init();
+  std::env::set_var("RUST_LOG", "actix_web=info");
+  env_logger::init();
 
-    // Create Juniper schema
-    let schema = std::sync::Arc::new(create_schema());
+  // Create Juniper schema
+  let schema = std::sync::Arc::new(create_schema());
 
-    // Start http server
-    HttpServer::new(move || {
-        App::new()
-            .data(schema.clone())
-            .wrap(middleware::Logger::default())
-            .service(web::resource("/graphql").route(web::post().to_async(graphql)))
-            .service(web::resource("/graphiql").route(web::get().to(graphiql)))
-    })
-    .bind("127.0.0.1:8080")?
-    .run()
+  // Start http server
+  HttpServer::new(move || {
+    App::new()
+      .data(schema.clone())
+      .wrap(middleware::Logger::default())
+      .service(web::resource("/graphql").route(web::post().to_async(graphql)))
+      .service(web::resource("/graphiql").route(web::get().to(graphiql)))
+  })
+  .bind("127.0.0.1:8080")?
+  .run()
 }
