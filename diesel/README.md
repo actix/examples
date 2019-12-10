b9b25ef3008e81520c92f62679ad15603132d8dd
Bevan Hunt <bevan@bevanhunt.com>
2019-12-10 07:56:08 -0800

    get example fully working

--- diesel/README.md
+++ diesel/README.md
@@ -1,46 +1,14 @@
-# diesel
+# Diesel Actix Web Example with Postgres
 
-Diesel's `Getting Started` guide using SQLite for Actix web
+## Run
 
-## Usage
+- install docker & docker-compose & make & curl
+- ``` make ```
 
-### init database sqlite
+## Add record via JSON and CURL (when running)
 
-```bash
-# if opensuse: sudo zypper install sqlite3-devel
-cargo install diesel_cli --no-default-features --features sqlite
-cd examples/diesel
-echo "DATABASE_URL=test.db" > .env
-diesel migration run
-```
+- ``` make add ```
 
-### server
+## Check Records
 
-```bash
-# if ubuntu : sudo apt-get install libsqlite3-dev
-# if fedora : sudo dnf install libsqlite3x-devel
-# if opensuse: sudo zypper install libsqlite3-0
-cd examples/diesel
-cargo run (or ``cargo watch -x run``)
-# Started http server: 127.0.0.1:8080
-```
-
-### web client
-
-[http://127.0.0.1:8080/NAME](http://127.0.0.1:8080/NAME)
-
-### sqlite client
-
-```bash
-# if ubuntu : sudo apt-get install sqlite3
-# if fedora : sudo dnf install sqlite3x
-# if opensuse: sudo zypper install sqlite3
-sqlite3 test.db
-sqlite> .tables
-sqlite> select * from users;
-```
-
-
-## Postgresql
-
-You will also find another complete example of diesel+postgresql on      [https://github.com/TechEmpower/FrameworkBenchmarks/tree/master/frameworks/Rust/actix](https://github.com/TechEmpower/FrameworkBenchmarks/tree/master/frameworks/Rust/actix)
+- open [localhost:5001](http://localhost:5001) and add server (host is postgres) and view records of users table
--- diesel/src/main.rs
+++ diesel/src/main.rs
@@ -1,32 +1,22 @@
-//! Actix web diesel example
-//!
-//! Diesel does not support tokio, so we have to run it in separate threads.
-//! Actix supports sync actors by default, so we going to create sync actor
-//! that use diesel. Technically sync actors are worker style actors, multiple
-//! of them can run in parallel and process messages from same queue.
 #[macro_use]
 extern crate diesel;
 #[macro_use]
 extern crate serde_derive;
 
-use actix_web::{error, middleware, web, App, Error, HttpResponse, HttpServer};
-use bytes::BytesMut;
+use actix_web::{middleware, web, App, Error, HttpResponse, HttpServer};
 use diesel::prelude::*;
 use diesel::r2d2::{self, ConnectionManager};
-use dotenv;
-use futures::StreamExt;
 
 mod models;
 mod schema;
 
-type Pool = r2d2::Pool<ConnectionManager<SqliteConnection>>;
+type Pool = r2d2::Pool<ConnectionManager<PgConnection>>;
 
 #[derive(Debug, Serialize, Deserialize)]
 struct MyUser {
     name: String,
 }
 
-/// Diesel query
 fn query(
     nm: String,
     pool: web::Data<Pool>,
@@ -38,7 +28,7 @@ fn query(
         id: &uuid,
         name: nm.as_str(),
     };
-    let conn: &SqliteConnection = &pool.get().unwrap();
+    let conn: &PgConnection = &pool.get().unwrap();
 
     diesel::insert_into(users).values(&new_user).execute(conn)?;
 
@@ -46,46 +36,13 @@ fn query(
     Ok(items.pop().unwrap())
 }
 
-/// Async request handler
-async fn add(
-    name: web::Path<String>,
-    pool: web::Data<Pool>,
-) -> Result<HttpResponse, Error> {
-    // run diesel blocking code
-    Ok(web::block(move || query(name.into_inner(), pool))
-        .await
-        .map(|user| HttpResponse::Ok().json(user))
-        .map_err(|_| HttpResponse::InternalServerError())?)
-}
-
-/// This handler manually parse json object. Bytes object supports FromRequest trait (extractor)
-/// and could be loaded from request payload automatically
-async fn index_add(body: Bytes, pool: web::Data<Pool>) -> Result<HttpResponse, Error> {
-    let mut body = BytesMut::new();
-    // body is loaded, now we can deserialize id with serde-json
-    let r_obj = serde_json::from_slice::<MyUser>(&body);
-
-    // Send to the db for create return response to peer
-    match r_obj {
-        Ok(obj) => {
-            let user = web::block(move || query(obj.name, pool))
-                .await
-                .map_err(|_| Error::from(HttpResponse::InternalServerError()))?;
-            Ok(HttpResponse::Ok().json(user))
-        }
-        Err(_) => Err(error::ErrorBadRequest("Json Decode Failed")),
-    }
-}
-
-/// This handler offloads json deserialization to actix-web's Json extrator
-async fn add2(
+async fn insert(
     item: web::Json<MyUser>,
     pool: web::Data<Pool>,
 ) -> Result<HttpResponse, Error> {
-    // run diesel blocking code
     let user = web::block(move || query(item.into_inner().name, pool))
         .await
-        .map_err(|_| HttpResponse::InternalServerError())?; // convert diesel error to http response
+        .map_err(|_| HttpResponse::InternalServerError())?;
 
     Ok(HttpResponse::Ok().json(user))
 }
@@ -93,47 +50,22 @@ async fn add2(
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     std::env::set_var("RUST_LOG", "actix_web=info");
-    env_logger::init();
-
-    dotenv::dotenv().ok();
-
     let connspec = std::env::var("DATABASE_URL").expect("DATABASE_URL");
-    let manager = ConnectionManager::<SqliteConnection>::new(connspec);
+    let manager = ConnectionManager::<PgConnection>::new(connspec);
     let pool = r2d2::Pool::builder()
         .build(manager)
         .expect("Failed to create pool.");
 
-    // Start http server
     HttpServer::new(move || {
         App::new()
             .data(pool.clone())
-            // enable logger
             .wrap(middleware::Logger::default())
-            // This can be called with:
-            // curl -S --header "Content-Type: application/json" --request POST --data '{"name":"xyz"}'  http://127.0.0.1:8080/add
-            // Use of the extractors makes some post conditions simpler such
-            // as size limit protections and built in json validation.
-            .service(
-                web::resource("/add2")
+            .service(web::resource("/")
                     .data(
                         web::JsonConfig::default()
-                            .limit(4096) // <- limit size of the payload
-                            .error_handler(|err, _| {
-                                // <- create custom error response
-                                error::InternalError::from_response(
-                                    err,
-                                    HttpResponse::Conflict().finish(),
-                                )
-                                .into()
-                            }),
                     )
-                    .route(web::post().to(add2)),
+                    .route(web::post().to(insert)),
             )
-            //  Manual parsing would allow custom error construction, use of
-            //  other parsers *beside* json (for example CBOR, protobuf, xml), and allows
-            //  an application to standardise on a single parser implementation.
-            .service(web::resource("/add").route(web::post().to(index_add)))
-            .service(web::resource("/add/{name}").route(web::get().to(add)))
     })
     .bind("0.0.0.0:3000")?
     .start()
