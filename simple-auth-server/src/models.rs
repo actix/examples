239164c47a3cf7c3d724b4b626b5ce9eeae46e43
Roman Frołow <rofrol@gmail.com>
2020-01-12 12:36:57 +0100

    Use derive feature from serde https://github.com/serde-rs/serde/issues/1441#issuecomment-445481084

--- simple-auth-server/src/models.rs
+++ simple-auth-server/src/models.rs
@@ -1,5 +1,6 @@
 use super::schema::*;
 use diesel::{r2d2::ConnectionManager, PgConnection};
+use serde::{Deserialize, Serialize};
 
 // type alias to use in multiple places
 pub type Pool = r2d2::Pool<ConnectionManager<PgConnection>>;
