6bb338a4b23bbb7f36a7495b5d1b213f45b98bba
Alexander Lyon <arlyon@me.com>
2020-02-11 23:29:23 +0000

    Make example compatible with juniper's new async feature

--- juniper/src/schema.rs
+++ juniper/src/schema.rs
@@ -1,5 +1,7 @@
-use juniper::FieldResult;
-use juniper::RootNode;
+use juniper::{
+    EmptySubscription, FieldResult, GraphQLEnum, GraphQLInputObject, GraphQLObject,
+    RootNode,
+};
 
 #[derive(GraphQLEnum)]
 enum Episode {
@@ -7,8 +9,6 @@ enum Episode {
     Empire,
     Jedi,
 }
-
-use juniper::{GraphQLEnum, GraphQLInputObject, GraphQLObject};
 
 #[derive(GraphQLObject)]
 #[graphql(description = "A humanoid creature in the Star Wars universe")]
@@ -29,11 +29,11 @@ struct NewHuman {
 
 pub struct QueryRoot;
 
-#[juniper::object]
+#[juniper::graphql_object]
 impl QueryRoot {
-    fn human(id: String) -> FieldResult<Human> {
+    async fn human(id: String) -> FieldResult<Human> {
         Ok(Human {
-            id: "1234".to_owned(),
+            id,
             name: "Luke".to_owned(),
             appears_in: vec![Episode::NewHope],
             home_planet: "Mars".to_owned(),
@@ -43,9 +43,9 @@ impl QueryRoot {
 
 pub struct MutationRoot;
 
-#[juniper::object]
+#[juniper::graphql_object]
 impl MutationRoot {
-    fn create_human(new_human: NewHuman) -> FieldResult<Human> {
+    async fn create_human(new_human: NewHuman) -> FieldResult<Human> {
         Ok(Human {
             id: "1234".to_owned(),
             name: new_human.name,
@@ -55,8 +55,8 @@ impl MutationRoot {
     }
 }
 
-pub type Schema = RootNode<'static, QueryRoot, MutationRoot>;
+pub type Schema = RootNode<'static, QueryRoot, MutationRoot, EmptySubscription>;
 
 pub fn create_schema() -> Schema {
-    Schema::new(QueryRoot {}, MutationRoot {})
+    Schema::new(QueryRoot {}, MutationRoot {}, EmptySubscription::new())
 }
