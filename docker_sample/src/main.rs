4666062b5a1a75583c6bbadf63563e2c483ef4af
curiousdev <bartonm09@gmail.com>
2020-02-24 10:48:04 -0700

    Add `docker_sample` to examples collection

---
+++ docker_sample/src/main.rs
@@ -0,0 +1,30 @@
+#[macro_use]
+extern crate actix_web;
+
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn index() -> impl Responder {
+    println!("GET: /");
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[get("/again")]
+async fn again() -> impl Responder {
+    println!("GET: /again");
+    HttpResponse::Ok().body("Hello world again!")
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    println!("Starting actix-web server");
+
+    HttpServer::new(|| {
+        App::new()
+            .service(index)
+            .service(again)
+    })
+    .bind("0.0.0.0:5000")?
+    .run()
+    .await
+}
