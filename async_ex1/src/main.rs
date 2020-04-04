6cdc1c873804439b8de13f634d020f19b33763f9
Yuki Okushi <huyuumi.dev@gmail.com>
2020-04-05 02:26:11 +0900

    part1: Update dependencies

--- async_ex1/src/main.rs
+++ async_ex1/src/main.rs
@@ -29,9 +29,9 @@ use validator_derive::Validate;
 
 #[derive(Debug, Validate, Deserialize, Serialize)]
 struct SomeData {
-    #[validate(length(min = "1", max = "1000000"))]
+    #[validate(length(min = 1, max = 1000000))]
     id: String,
-    #[validate(length(min = "1", max = "100"))]
+    #[validate(length(min = 1, max = 100))]
     name: String,
 }
 
