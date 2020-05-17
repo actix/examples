eb87f5fef8a33b7f8d7fea409a095aeb7807b68b
Pieter Frenssen <pieter@frenssen.be>
2020-05-17 14:19:02 +0300

    Improve formatting.

--- static_index/src/main.rs
+++ static_index/src/main.rs
@@ -10,18 +10,13 @@ async fn main() -> std::io::Result<()> {
         App::new()
             // Enable the logger.
             .wrap(middleware::Logger::default())
-
             // We allow the visitor to see an index of the images at `/images`.
-            .service(
-                Files::new("/images", "static/images/").show_files_listing(),
-            )
+            .service(Files::new("/images", "static/images/").show_files_listing())
             // Serve a tree of static files at the web root and specify the index file.
             // Note that the root path should always be defined as the last item. The paths are
             // resolved in the order they are defined. If this would be placed before the `/images`
             // path then the service for the static images would never be reached.
-            .service(
-                Files::new("/", "./static/root/").index_file("index.html"),
-            )
+            .service(Files::new("/", "./static/root/").index_file("index.html"))
     })
     .bind("127.0.0.1:8080")?
     .run()
