7823473f8d74b09a492665d1e32488bdb67d6670
Yuki Okushi <huyuumi.dev@gmail.com>
2020-05-19 13:48:49 +0900

    Sanitize filename

--- multipart/src/main.rs
+++ multipart/src/main.rs
@@ -9,7 +9,7 @@ async fn save_file(mut payload: Multipart) -> Result<HttpResponse, Error> {
     while let Ok(Some(mut field)) = payload.try_next().await {
         let content_type = field.content_disposition().unwrap();
         let filename = content_type.get_filename().unwrap();
-        let filepath = format!("./tmp/{}", filename);
+        let filepath = format!("./tmp/{}", sanitize_filename::sanitize(&filename));
         // File::create is blocking operation, use threadpool
         let mut f = web::block(|| std::fs::File::create(filepath))
             .await
