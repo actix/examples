b7d1602028037c9850e6cae880dcd24fd90b98cf
James T <github.jtara1@gmail.com>
2021-06-20 15:32:36 -0700

    feat: added env vars HOST, PORT for specifying the address at which this app is bound to

--- websockets/chat-broker/src/main.rs
+++ websockets/chat-broker/src/main.rs
@@ -22,16 +22,18 @@ async fn main() -> std::io::Result<()> {
     env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("debug"))
         .init();
 
-    let addr = "127.0.0.1:8080";
+    let host = std::env::var("HOST").unwrap_or(String::from("127.0.0.1"));
+    let port = std::env::var("PORT").unwrap_or(String::from("8080"));
+    let address = format!("{}:{}", &host, &port);
 
     let srv = HttpServer::new(move || {
         App::new()
             .service(web::resource("/ws/").to(chat_route))
             .service(Files::new("/", "./static/").index_file("index.html"))
     })
-    .bind(&addr)?;
+        .bind(&address)?;
 
-    info!("Starting http server: {}", &addr);
+    info!("Starting http server: {}", &address);
 
     srv.run().await
 }
