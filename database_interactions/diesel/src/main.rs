261589ca87e2e36f50bfd0a9b69e63cbbbcf1bc1
fakeshadow <24548779@qq.com>
2021-10-22 15:47:12 +0800

    fix diesel example (#460)

--- database_interactions/diesel/src/main.rs
+++ database_interactions/diesel/src/main.rs
@@ -24,15 +24,17 @@ async fn get_user(
     user_uid: web::Path<Uuid>,
 ) -> Result<HttpResponse, Error> {
     let user_uid = user_uid.into_inner();
-    let conn = pool.get().expect("couldn't get db connection from pool");
 
     // use web::block to offload blocking Diesel code without blocking server thread
-    let user = web::block(move || actions::find_user_by_uid(user_uid, &conn))
-        .await
-        .map_err(|e| {
-            eprintln!("{}", e);
-            HttpResponse::InternalServerError().finish()
-        })?;
+    let user = web::block(move || {
+        let conn = pool.get()?;
+        actions::find_user_by_uid(user_uid, &conn)
+    })
+    .await
+    .map_err(|e| {
+        eprintln!("{}", e);
+        HttpResponse::InternalServerError().finish()
+    })?;
 
     if let Some(user) = user {
         Ok(HttpResponse::Ok().json(user))
@@ -49,15 +51,16 @@ async fn add_user(
     pool: web::Data<DbPool>,
     form: web::Json<models::NewUser>,
 ) -> Result<HttpResponse, Error> {
-    let conn = pool.get().expect("couldn't get db connection from pool");
-
     // use web::block to offload blocking Diesel code without blocking server thread
-    let user = web::block(move || actions::insert_new_user(&form.name, &conn))
-        .await
-        .map_err(|e| {
-            eprintln!("{}", e);
-            HttpResponse::InternalServerError().finish()
-        })?;
+    let user = web::block(move || {
+        let conn = pool.get()?;
+        actions::insert_new_user(&form.name, &conn)
+    })
+    .await
+    .map_err(|e| {
+        eprintln!("{}", e);
+        HttpResponse::InternalServerError().finish()
+    })?;
 
     Ok(HttpResponse::Ok().json(user))
 }
