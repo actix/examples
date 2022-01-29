5df5bd10eab6216391e434384c38683c66c23c06
Christopher Gubbin <44929740+cgubbin@users.noreply.github.com>
2022-01-29 16:40:50 +0000

    Updated forms/multipart to v4. (#503)

--- forms/multipart/src/main.rs
+++ forms/multipart/src/main.rs
@@ -9,23 +9,20 @@ async fn save_file(mut payload: Multipart) -> Result<HttpResponse, Error> {
     // iterate over multipart stream
     while let Some(mut field) = payload.try_next().await? {
         // A multipart/form-data stream has to contain `content_disposition`
-        let content_disposition = field
-            .content_disposition()
-            .ok_or_else(|| HttpResponse::BadRequest().finish())?;
+        let content_disposition = field.content_disposition();
 
-        let filename = content_disposition.get_filename().map_or_else(
-            || Uuid::new_v4().to_string(),
-            |f| sanitize_filename::sanitize(f),
-        );
+        let filename = content_disposition
+            .get_filename()
+            .map_or_else(|| Uuid::new_v4().to_string(), sanitize_filename::sanitize);
         let filepath = format!("./tmp/{}", filename);
 
         // File::create is blocking operation, use threadpool
-        let mut f = web::block(|| std::fs::File::create(filepath)).await?;
+        let mut f = web::block(|| std::fs::File::create(filepath)).await??;
 
         // Field in turn is stream of *Bytes* object
         while let Some(chunk) = field.try_next().await? {
             // filesystem operations are blocking, we have to use threadpool
-            f = web::block(move || f.write_all(&chunk).map(|_| f)).await?;
+            f = web::block(move || f.write_all(&chunk).map(|_| f)).await??;
         }
     }
 
