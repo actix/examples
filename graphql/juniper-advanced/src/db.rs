3ffb06abb7beffb5405ef2a8f2625954c0ac5f86
Rob Ede <robjtede@icloud.com>
2022-12-11 15:25:15 +0000

    update mysql dependency to 23

--- graphql/juniper-advanced/src/db.rs
+++ graphql/juniper-advanced/src/db.rs
@@ -1,14 +1,14 @@
 use r2d2_mysql::{
     mysql::{Opts, OptsBuilder},
-    MysqlConnectionManager,
+    MySqlConnectionManager,
 };
 
-pub type Pool = r2d2::Pool<MysqlConnectionManager>;
+pub type Pool = r2d2::Pool<MySqlConnectionManager>;
 
 pub fn get_db_pool() -> Pool {
     let db_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     let opts = Opts::from_url(&db_url).unwrap();
     let builder = OptsBuilder::from_opts(opts);
-    let manager = MysqlConnectionManager::new(builder);
+    let manager = MySqlConnectionManager::new(builder);
     r2d2::Pool::new(manager).expect("Failed to create DB Pool")
 }
