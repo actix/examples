74b794db2fea4fd6754d4dbdcb955c294a4d962d
d0bhareach <viacheslav.modestov@gmail.com>
2022-12-29 12:56:21 +0100

    Fix auth/cookie-auth, add time to live to SessionMiddleware

--- auth/cookie-auth/src/main.rs
+++ auth/cookie-auth/src/main.rs
@@ -1,15 +1,20 @@
 use actix_identity::{Identity, IdentityMiddleware};
-use actix_session::{storage::CookieSessionStore, SessionMiddleware};
+use actix_session::{config::PersistentSession, storage::CookieSessionStore, SessionMiddleware};
 use actix_web::{
-    cookie::Key, middleware, web, App, HttpMessage as _, HttpRequest, HttpResponse, HttpServer,
+    cookie::{time::Duration, Key},
+    middleware, web, App, HttpMessage as _, HttpRequest, HttpResponse, HttpServer,
 };
-async fn index(id: Identity) -> String {
-    format!(
-        "Hello {}",
-        id.id().unwrap_or_else(|_| "Anonymous".to_owned())
-    )
-}
 
+async fn index(id: Option<Identity>) -> String {
+    if let Some(id) = id {
+        format!(
+            "Hello {}",
+            id.id().unwrap_or_else(|_| "Identity Id Error.".to_owned())
+        )
+    } else {
+        "Hello Anonymous!".to_owned()
+    }
+}
 async fn login(req: HttpRequest) -> HttpResponse {
     Identity::login(&req.extensions(), "user1".to_owned()).unwrap();
 
@@ -35,19 +40,32 @@ async fn main() -> std::io::Result<()> {
     // secret key for every project. Anyone with access to the key can generate
     // authentication cookies for any user!
     let secret_key = Key::generate();
+    //
+    // If Key is read from the file, see commented line below.
+    // for random string below use: openssl rand -base64 64
+    // const PRIVATE_KEY: &str = "yD508YGdEzkgK48ysgdNX65FbOmf2I5z3jINtpOCSJ8qSP0fNz9uoonCwu9gplh0
+    // YpAFfvYLRLtBUtECZmRJqQ==";
+    const JUST_A_MINUTE: i64 = 60;
 
     HttpServer::new(move || {
         App::new()
             .wrap(IdentityMiddleware::default())
             .wrap(
-                SessionMiddleware::builder(CookieSessionStore::default(), secret_key.clone())
-                    .cookie_name("auth-example".to_owned())
-                    .cookie_secure(false)
-                    .build(),
+                SessionMiddleware::builder(
+                    CookieSessionStore::default(),
+                    // Key::from(PRIVATE_KEY.as_bytes())
+                    secret_key.clone(),
+                )
+                .cookie_name("auth-example".to_owned())
+                .cookie_secure(false)
+                .session_lifecycle(
+                    PersistentSession::default().session_ttl(Duration::seconds(JUST_A_MINUTE)),
+                )
+                .build(),
             )
             // enable logger - always register Actix Web Logger middleware last
             .wrap(middleware::Logger::default())
-            .service(web::resource("/login").route(web::post().to(login)))
+            .service(web::resource("/login").route(web::get().to(login)))
             .service(web::resource("/logout").to(logout))
             .service(web::resource("/").route(web::get().to(index)))
     })
