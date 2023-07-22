df118d87e48fc25c259ca44fe47c190087cad33e
Rob Ede <robjtede@icloud.com>
2023-07-22 22:26:46 +0100

    fix used fields

--- databases/postgres/src/main.rs
+++ databases/postgres/src/main.rs
@@ -57,9 +57,9 @@ mod db {
     use crate::{errors::MyError, models::User};
 
     pub async fn get_users(client: &Client) -> Result<Vec<User>, MyError> {
-        let _stmt = include_str!("../sql/get_users.sql");
-        let _stmt = _stmt.replace("$table_fields", &User::sql_table_fields());
-        let stmt = client.prepare(&_stmt).await.unwrap();
+        let stmt = include_str!("../sql/get_users.sql");
+        let stmt = stmt.replace("$table_fields", &User::sql_table_fields());
+        let stmt = client.prepare(&stmt).await.unwrap();
 
         let results = client
             .query(&stmt, &[])
